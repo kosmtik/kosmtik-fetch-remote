7e172392ff9ab7d34141f92dad0bf99e595d1ffb
Yohan Boniface <yb@enix.org>
2021-02-06 12:23:02 +0100

    Basic way of supporting other file types than shape (geojson, csv…)

--- index.js
+++ index.js
@@ -6,6 +6,78 @@ function log() {
     console.warn.apply(console, Array.prototype.concat.apply(['[Fetch Remote]'], arguments));
 };
 
+
+class Fetch {
+    constructor(config, layer, dataDir, callback) {
+        this.config = config
+        this.layer = layer
+        this.callback = callback
+        this.uri = layer.Datasource.file
+        this.basename = path.basename(this.uri)
+        this.destDir = path.join(dataDir, layer.id)
+        this.dest = path.join(this.destDir, this.basename)
+        try {
+            fs.mkdirSync(this.destDir);
+        } catch (err) {}
+        layer.Datasource.file = this.dest
+        this.patchLayer()
+        log('New file for layer', layer.id, '=>', layer.Datasource.file)
+    }
+
+    patchLayer () {
+        this.layer.Datasource.file = this.dest;
+    }
+
+    onDownloaded () {}
+
+    onError (err) {
+        log('Error while fetching', this.uri);
+        log(err.message);
+        fs.unlink(this.dest);
+        this.callback();
+    }
+
+    onResponse (resp) {
+        if (resp.statusCode >= 400) return onError(new Error('Bad status code: ' + resp.statusCode));
+        var file = fs.createWriteStream(this.dest);
+        resp.pipe(file);
+        file.on('finish', function onFinish() {
+            file.close(this.onDownloaded);
+        });
+    }
+
+
+    download (force) {
+        if(fs.existsSync(this.dest) && !force) {
+            log('File already exists and not force mode', this.dest, 'SKIPPING');
+            return this.callback();
+        }
+        this.config.helpers.request({uri: this.uri}).on('error', this.onError.bind(this)).on('response', this.onResponse.bind(this));
+    }
+
+}
+
+
+class FetchShp extends Fetch {
+
+    patchLayer () {
+        this.layer.Datasource.file = path.join(this.destDir, this.layer.id);
+    }
+
+    onDownloaded () {
+        fs.createReadStream(this.dest)
+        .pipe(unzip.Parse())
+        .on('entry', function (entry) {
+            var fileName = entry.path;
+            log(fileName);
+            if (entry.type === 'Directory') return entry.autodrain();
+            if (this.layer.Datasource.type === 'shape') fileName = this.layer.id + path.extname(fileName);
+            entry.pipe(fs.createWriteStream(path.join(this.destDir, fileName)));
+        }).on('close', this.callback);
+    }
+
+}
+
 class FetchRemote {
     constructor(config) {
         config.commands.serve.option('force-fetch-remote', {help: 'Refetch overpass data even if local file exists [Default: false]', flag: true});
@@ -14,9 +86,8 @@ class FetchRemote {
 
     patchMML(e) {
         if (!e.project.mml || !e.project.mml.Layer) return e.continue();
-        var processed = 0, layer,
+        var processed = 0, layer, klass, downloader,
             force = this.config.parsed_opts['force-fetch-remote'],
-            self = this,
             incr = function () {
                 processed++;
             },
@@ -30,58 +101,23 @@ class FetchRemote {
                 log('Done.');
                 e.continue();
             };
-        var download = function (layer) {
-            incr();
-            var uri = layer.Datasource.file,
-                basename = path.basename(uri),
-                destDir = path.join(e.project.dataDir, layer.id),
-                dest = path.join(destDir, basename);
-            try {
-                fs.mkdirSync(destDir);
-            } catch (err) {}
-            layer.Datasource.file = path.join(destDir, layer.id);
-            log('New file for layer', layer.id, '=>', layer.Datasource.file);
-            if(fs.existsSync(dest) && !force) {
-                log('File already exists and not force mode', dest, 'SKIPPING');
-                return decr();
-            }
-            var onDownloaded = function () {
-                fs.createReadStream(dest)
-                .pipe(unzip.Parse())
-                .on('entry', function (entry) {
-                    var fileName = entry.path;
-                    log(fileName);
-                    if (entry.type === 'Directory') return entry.autodrain();
-                    if (layer.Datasource.type === 'shape') fileName = layer.id + path.extname(fileName);
-                    entry.pipe(fs.createWriteStream(path.join(destDir, fileName)));
-                }).on('close', decr);
-            };
-            var onError = function (err) {
-                log('Error while fetching', uri);
-                log(err.message);
-                fs.unlink(dest);
-                decr();
-            };
-            var onResponse = function (resp) {
-                if (resp.statusCode >= 400) return onError(new Error('Bad status code: ' + resp.statusCode));
-                var file = fs.createWriteStream(dest);
-                resp.pipe(file);
-                file.on('finish', function onFinish() {
-                    file.close(onDownloaded);
-                });
-            };
-            self.config.helpers.request({uri: uri}).on('error', onError).on('response', onResponse);
-        };
         incr();
         for (var i = 0; i < e.project.mml.Layer.length; i++) {
             layer = e.project.mml.Layer[i];
             if (layer.Datasource && layer.Datasource.file && layer.Datasource.file.search('^https?://') === 0) {
                 log('Processing file', layer.Datasource.file);
-                download(layer);
+                klass = downloaders[layer.Datasource.type] || Fetch
+                downloader = new klass(this.config, layer, e.project.dataDir, decr)
+                incr()
+                downloader.download(force);
             }
         }
         decr();
     }
+}
+
+const downloaders = {
+    "shape": FetchShp,
 }
 
 exports = module.exports = { Plugin: FetchRemote };
